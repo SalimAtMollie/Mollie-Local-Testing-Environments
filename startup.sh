1be22e30743967887d844aa397c664d55bd46c22
Salim Bouziane Rabhi <salim.bouziane.rabhi@mollie.com>
2022-07-27 16:04:47 +0200

    Cleanup

--- startup.sh
+++ startup.sh
@@ -53,7 +53,6 @@ if [ ! -f ./${webshops[$num]}/docker-compose.yml ]
 then
     echo "${RED}[!]" ${webshops[$num]} "is not downloaded.${NC}"
     read -n1 -r -p "${YELLOW}[?] Would you like to download it?${NC} (${GREEN}y${NC}/${RED}n${NC}): " dwnld
-    echo $dwnld
     if [ "$dwnld" = "y" ] || [ "$dwnld" = "Y" ]; then #Download
         echo "${GREEN}[!] Downloading ${webshops[$num]}...${NC}"
         git clone https://github.com/SalimAtMollie/${webshops[$num]}xMollie ${webshops[$num]} #Download repo from github
